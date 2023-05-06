41afb09f45615441e7d992ac5471ef170866eb03
김종범 <jaybkim02@gmail.com>
2023-05-06 11:31:59 +0900

    fix: 회원가입  feat: viewCount

---
+++ src/main/java/com/sparta/hanghae66/entity/User.java
@@ -0,0 +1,36 @@
+package com.sparta.hanghae66.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity(name="USERS")
+@NoArgsConstructor
+public class User extends Timestamped {
+    @Id
+    private String Id;
+
+    private String username;
+
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRole role;
+
+    @Column(nullable = false)
+    private int userSkill;
+
+    @Column(nullable = false)
+    private int userYear;
+
+    public User(String username, String password, UserRole role, int userSkill, int userYear) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.userSkill = userSkill;
+        this.userYear = userYear;
+    }
+
+}
