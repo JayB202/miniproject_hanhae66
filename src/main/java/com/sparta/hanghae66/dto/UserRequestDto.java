902eb96619422b15c88b9469ea978ab0d1bc9e43
tiktokyi <tiktokyi@gmail.com>
2023-05-07 13:55:04 +0900

    refactor(spring) : UserRequestDto 리팩토링

--- src/main/java/com/sparta/hanghae66/dto/UserRequestDto.java
+++ src/main/java/com/sparta/hanghae66/dto/UserRequestDto.java
@@ -13,9 +13,13 @@ import lombok.NoArgsConstructor;
 public class UserRequestDto {
 
     @Size(min = 4, max = 10, message = "id는 4 이상, 10 이하만 가능합니다.")
-    @Pattern(regexp = "^[a-z0-9]+$", message = "id는 소문자, 숫자만 가능합니다.")
+    @Pattern(regexp = "^[a-z0-9]+$", message = "소문자와 숫자만 입력 가능합니다. 아이디를 입력해주세요.")
     @NotNull(message = "id를 입력해주세요")
-    private String username;
+    private String userId;
+
+    @Size(min = 1, max = 10, message = "닉네임은 크기가 최소 1 이상, 10 이하만 가능합니다.")
+    @NotNull(message = "닉네임을 입력해주세요")
+    private String userName;
 
     @Size(min = 8, max = 15, message = "password는 8 이상, 15 이하만 가능합니다.")
     @Pattern(regexp = "^[a-zA-Z\\p{Punct}0-9]*$", message = "password는 알파벳 대소문자, 특수문자, 숫자만 가능합니다.")
@@ -27,6 +31,6 @@ public class UserRequestDto {
 
     private int userSkill;
 
-    private int userYear;
+    private Long userYear;
 
 }
