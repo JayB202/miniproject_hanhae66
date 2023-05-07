017766e24ba920fa01726b2e9bae5641b3cdaf74
tiktokyi <tiktokyi@gmail.com>
2023-05-07 13:48:00 +0900

    refactor(spring) : CommentService 리팩토링

--- src/main/java/com/sparta/hanghae66/service/CommentService.java
+++ src/main/java/com/sparta/hanghae66/service/CommentService.java
@@ -1,11 +1,10 @@
 package com.sparta.hanghae66.service;
 
 import com.sparta.hanghae66.dto.ResponseDto;
-import com.sparta.hanghae66.entity.Post;
 import com.sparta.hanghae66.entity.Comment;
-import com.sparta.hanghae66.entity.CommentLikes;
-import com.sparta.hanghae66.entity.UserRole;
+import com.sparta.hanghae66.entity.Post;
 import com.sparta.hanghae66.entity.User;
+import com.sparta.hanghae66.entity.UserRole;
 import com.sparta.hanghae66.repository.CommentLikesRepository;
 import com.sparta.hanghae66.repository.CommentRepository;
 import com.sparta.hanghae66.repository.PostRepository;
@@ -13,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
@@ -26,9 +26,9 @@ public class CommentService {
 
     @Transactional
     public ResponseDto createComment(Long postId, String content, User user) {
-        Post post = postCheck(postId);
+        Post post = findPost(postId);
 
-        Comment comment = new Comment(content, user.getUsername());
+        Comment comment = new Comment(content, user.getUserName());
         post.addComment(comment);
 
         commentRepository.save(comment);
@@ -37,42 +37,71 @@ public class CommentService {
     }
 
     @Transactional
-    public ResponseDto  updateComment(Long commentId, User user) {
+    public ResponseDto updateComment(Long commentId, User user) {
+        try {
+            Comment comment = findComment(commentId);
+            UserRole userRole = user.getRole();
 
-        Comment comment = commentCheck(commentId);
-
-        if (comment.getUserId().equals(user.getId()) || user.getRole().equals(UserRole.ADMIN)) {
-            comment.update(comment.getContent());
-            return new ResponseDto("댓글 수정 완료.", HttpStatus.OK);
-        } else {
-            return new ResponseDto("댓글 수정 실패.", HttpStatus.BAD_REQUEST);
+            switch (userRole) {
+                case USER:
+                    if (StringUtils.pathEquals(comment.getCmtUserId(), user.getId())) {
+                        commentRepository.delete(comment);
+                        return new ResponseDto("삭제완료", HttpStatus.OK);
+                    }
+                case ADMIN:
+                    commentRepository.delete(comment);
+                    return new ResponseDto("삭제완료", HttpStatus.OK);
+                default:
+                    return null;
+            }
+        } catch (Exception ex) {
+            throw ex;
         }
     }
 
     @Transactional
     public ResponseDto deleteComment(Long commentId, User user) {
 
-        Comment comment = commentCheck(commentId);
+        try {
+            Comment comment = findComment(commentId);
+            UserRole userRole = user.getRole();
 
-        if (comment.getUserId().equals(user.getId()) || user.getRole().equals(UserRole.ADMIN)) {
-            List<CommentLikes> commentLikes = commentLikesRepository.findAll();
-            commentLikesRepository.deleteAll(commentLikes);
-            commentRepository.delete(comment);
-            return new ResponseDto("댓글 삭제 완료.", HttpStatus.OK);
-        } else {
-            return new ResponseDto("댓글 삭제 실패.", HttpStatus.BAD_REQUEST);
+            switch (userRole) {
+                case USER:
+                    if (StringUtils.pathEquals(comment.getCmtUserId(), user.getId())) {
+                        commentRepository.delete(comment);
+                        return new ResponseDto("삭제완료", HttpStatus.OK);
+                    }
+                case ADMIN:
+                    commentRepository.delete(comment);
+                    return new ResponseDto("삭제완료", HttpStatus.OK);
+                default:
+                    return null;
+            }
+        } catch (Exception ex) {
+            throw ex;
         }
     }
 
-    public Post postCheck(Long postId) {
+    @Transactional(readOnly = true)
+        public List<Comment> myCommentList(User user) {
+            List<Comment> commentList = commentRepository.findByCommentUserId(user.getId());
+            return commentList;
+        }
+
+    @Transactional(readOnly = true)
+    public Post findPost(Long postId) {
         return postRepository.findById(postId).orElseThrow(
                 () -> new IllegalArgumentException("존재하지 않는 게시물입니다.")
         );
     }
 
-    public Comment commentCheck(Long commentId) {
+    @Transactional(readOnly = true)
+    public Comment findComment(Long commentId) {
         return commentRepository.findById(commentId).orElseThrow(
                 () -> new IllegalArgumentException("해당 댓글이 존재하지 않습니다.")
         );
     }
+
+
 }
