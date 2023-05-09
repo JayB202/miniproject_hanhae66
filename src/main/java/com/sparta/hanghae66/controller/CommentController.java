29d4ad62e7ff507f89bddd0f066ae0e9be1fa1f4
김종범 <jaybkim02@gmail.com>
2023-05-09 14:18:07 +0900

    feat(spring): swagger import

--- src/main/java/com/sparta/hanghae66/controller/CommentController.java
+++ src/main/java/com/sparta/hanghae66/controller/CommentController.java
@@ -2,14 +2,20 @@ package com.sparta.hanghae66.controller;
 
 import com.sparta.hanghae66.dto.CommentRequestDto;
 import com.sparta.hanghae66.dto.ResponseDto;
-import com.sparta.hanghae66.entity.Comment;
 import com.sparta.hanghae66.security.UserDetailsImpl;
 import com.sparta.hanghae66.service.CommentService;
 import com.sparta.hanghae66.service.LikeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+
+
+@Tag(name = "CommentController", description = "댓글 Controller")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("comment")
@@ -18,22 +24,30 @@ public class CommentController {
     private final CommentService commentService;
     private final LikeService likeService;
 
+    @Operation(summary = "댓글 작성 API" , description = "댓글 생성")
+    @ApiResponses(value ={@ApiResponse(responseCode= "200", description = "댓글 작성 완료" )})
     @PostMapping("{postId}")
     public ResponseDto createComment(@PathVariable Long postId, @RequestBody CommentRequestDto commentRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return commentService.createComment(postId, commentRequestDto.getCmtContent(), userDetails.getUser());
 
     }
 
+    @Operation(summary = "댓글 수정 API" , description = "댓글 수정")
+    @ApiResponses(value ={@ApiResponse(responseCode= "200", description = "댓글 수정 완료" )})
     @PutMapping("{commentId}")
     public ResponseDto updateComment(@PathVariable Long commentId, @RequestBody CommentRequestDto commentRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return commentService.updateComment(commentId, commentRequestDto, userDetails.getUser());
     }
 
+    @Operation(summary = "댓글 삭제 API" , description = "댓글 삭제")
+    @ApiResponses(value ={@ApiResponse(responseCode= "200", description = "댓글 삭제 완료" )})
     @DeleteMapping("{commentId}")
     public ResponseDto  deleteComment(@PathVariable Long commentId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return commentService.deleteComment(commentId, userDetails.getUser());
     }
 
+    @Operation(summary = "댓글 좋아요 API" , description = "댓글 좋아요 생성")
+    @ApiResponses(value ={@ApiResponse(responseCode= "200", description = "댓글 좋아요" )})
     @PostMapping("like/{commentId}")
     public ResponseDto commentLikeService(@PathVariable Long commentId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return likeService.commentLikeService(commentId, userDetails.getUser());
--- src/main/java/com/sparta/hanghae66/controller/MyPageController.java
+++ src/main/java/com/sparta/hanghae66/controller/MyPageController.java
@@ -3,18 +3,24 @@ package com.sparta.hanghae66.controller;
 import com.sparta.hanghae66.dto.MyPageResponseDto;
 import com.sparta.hanghae66.security.UserDetailsImpl;
 import com.sparta.hanghae66.service.MyPageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "MyPageController", description = "내 화면 Controller")
 @RestController
 @RequiredArgsConstructor
 public class MyPageController {
 
     private final MyPageService myPageService;
-
+    @Operation(summary = "마이페이지 API" , description = "마이페이지에 필요한 정보 리스폰스")
+    @ApiResponses(value ={@ApiResponse(responseCode= "200", description = "유저정보, 포스트리스트, 댓글 리스트 보냄" )})
     @GetMapping("/mypage/{userId}")
     public MyPageResponseDto mypage(@PathVariable String userId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return myPageService.mypage(userId);
